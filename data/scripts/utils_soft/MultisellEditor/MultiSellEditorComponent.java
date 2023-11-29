23a00feadf5c64c54810fc88758835a77acd3357
FuzzY <chipercualexandru@gmail.com>
2023-11-29 21:04:47 +0300

    refactoring: MultiSellEditorService- main-page

--- data/scripts/utils_soft/MultisellEditor/MultiSellEditorComponent.java
+++ data/scripts/utils_soft/MultisellEditor/MultiSellEditorComponent.java
@@ -4,10 +4,12 @@ import l2open.common.Html_Constructor.tags.Button;
 import l2open.common.Html_Constructor.tags.Table;
 import l2open.gameserver.model.L2Multisell;
 import l2open.gameserver.model.L2Player;
+import l2open.util.ArrayUtils;
 import utils_soft.common.Component;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import static l2open.common.Html_Constructor.tags.parameters.Parameters.*;
@@ -24,28 +26,30 @@ public class MultiSellEditorComponent extends Component {
 
 
     public void showMainPage(L2Player player, String[] args){
-
         int page = Integer.parseInt(args[1]);
-
         final ArrayList<MultiSellListContainer> containers = new ArrayList<>(multisell.getLists().values());
+        containers.sort(Comparator.comparingInt(MultiSellListContainer::getListId));
         final List<MultiSellListContainer> containerList = partitionList(containers, 20).get(page - 1);
 
         final Table main = new Table(2, 1).setParams(width(DEFAULT_TWINDOW_WIDTH));
         final Table pages = new Table(1, 3);
-        pages.row(0).col(0).setParams(width(150), height(20)).insert("prev");
-        pages.row(0).col(0).setParams(width(90)).insert(String.valueOf(page));
-        pages.row(0).col(0).setParams(width(150)).insert("next");
+        pages.row(0).col(0).setParams(width(100), height(20)).insert(new Button("<<", action("bypass -h admin_multisell_editor " + (page == 1? page: page -1)), 80, 20).build());
+        pages.row(0).col(1).setParams(width(80)).insert(String.valueOf(page));
+        pages.row(0).col(2).setParams(width(100)).insert(new Button(">>", action("bypass -h admin_multisell_editor " + (page > containerList.size() ? page: page + 1)), 80, 20).build());
 
-        final Table table = new Table(containerList.size(), 3).setParams(border(1));
-        table.row(0).col(0).setParams(width(100), height(20)).insert("id");
-        table.row(0).col(1).setParams(width(80)).insert("");
-        table.row(0).col(2).setParams(width(80)).insert("");
-        for (int i = 1; i < containerList.size(); i++) {
-            final MultiSellListContainer container = containerList.get(i - 1);
-            table.row(i).col(0).insert(new Button(String.valueOf(container.getListId()), action("bypass -h admin_multisell_editor_show_multisell " + container.getListId()), 80, 20).build(), true);
-            table.row(i).col(1).insert(new Button("redact", action(""), 60, 20).build());
-            table.row(i).col(2).insert(new Button("restore", action(""), 60, 20).build());
+        final Table table = new Table(containerList.size() + 1, 1);
+        for (int i = 0; i < containerList.size(); i++) {
+            final Table containerTable = new Table(2, 1);
+            final Table multiSellTable = new Table(1, 3);
+            final MultiSellListContainer container = containerList.get(i);
+            multiSellTable.row(0).col(0).insert(new Button(String.valueOf(container.getListId()), action("bypass -h admin_multisell_editor_show_multisell " + container.getListId()), 80, 20).build(), true);
+            multiSellTable.row(0).col(1).insert(new Button("redact", action(""), 60, 20).build());
+            multiSellTable.row(0).col(2).insert(new Button("restore", action(""), 60, 20).build());
+            containerTable.row(0).col(0).insert(multiSellTable.build());
+            containerTable.row(1).col(0).insert(separator(300));
+            table.row(i).col(0).insert(containerTable.build());
         }
+        table.row(containerList.size()).col(0).setParams(height(20)).insert("<br> ");
 
         main.row(0).col(0).insert(pages.build());
         main.row(1).col(0).insert(table.build());
@@ -54,7 +58,15 @@ public class MultiSellEditorComponent extends Component {
     }
 
 
-    public void showMultiSell(L2Player player, String[] args){
+
+
+
+
+
+
+
+
+    public void openMultiSell(L2Player player, String[] args){
         final int id = Integer.parseInt(args[1]);
         L2Multisell.getInstance().SeparateAndSend(id, player, 0);
     }
--- data/scripts/utils_soft/common/Component.java
+++ data/scripts/utils_soft/common/Component.java
@@ -1,6 +1,7 @@
 package utils_soft.common;
 
 import l2open.common.Html_Constructor.tags.Button;
+import l2open.common.Html_Constructor.tags.Img;
 import l2open.common.Html_Constructor.tags.Table;
 import l2open.common.Html_Constructor.tags.parameters.Position;
 import l2open.gameserver.model.L2Player;
@@ -13,7 +14,7 @@ import static l2open.common.Html_Constructor.tags.parameters.Parameters.*;
 
 public class Component {
 
-    protected static int DEFAULT_TWINDOW_WIDTH = 390;
+    protected static int DEFAULT_TWINDOW_WIDTH = 350;
 
     protected void showTWindow(L2Player player, String html, String title){
         player.sendPacket(new TutorialShowHtml("<html><body><title>" + title + "</title>" + TWindowCloseButton() + html + " </body></html>"));
@@ -24,8 +25,10 @@ public class Component {
     }
 
     private String TWindowCloseButton(){
-        final Table main = new Table(1, 1).setParams(width(DEFAULT_TWINDOW_WIDTH));
-        main.row(0).col(0).setParams(valign(Position.RIGHT)).insert(new Button(action("bypass -h TE00"), 16, 16, "L2UI_CT1.Button_DF_Delete_Down", "L2UI_CT1.Button_DF_Delete").build(), true);
+        final Table main = new Table(1, 3).setParams(fixwidth(DEFAULT_TWINDOW_WIDTH));
+        main.row(0).col(0).setParams(width(140));
+        main.row(0).col(1).setParams(width(140));
+        main.row(0).col(2).setParams(width(40)).insert(new Button(action("bypass -h TE00"), 16, 16, "L2UI_CT1.Button_DF_Delete_Down", "L2UI_CT1.Button_DF_Delete").build(), true);
         return main.build();
     }
 
@@ -38,6 +41,10 @@ public class Component {
         }
 
         return partitions;
+    }
+
+    public static String separator(int width){
+        return new Img("l2ui.squaregray", width, 1).build();
     }
 
 
