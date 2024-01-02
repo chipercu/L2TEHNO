98d5cbc55695eee82d6d1c4073c956f4b57cccda
FuzzY <chipercualexandru@gmail.com>
2024-01-02 13:56:02 +0300

    NpcEditorUtils.updateNpcStat

--- data/scripts/utils_soft/NpcEditor/NpcEditorUtils.java
+++ data/scripts/utils_soft/NpcEditor/NpcEditorUtils.java
@@ -38,30 +38,30 @@ public class NpcEditorUtils {
         }
     }
 
-    public static void updateNpcStat(int npcId, String stat, Object value){
+    public static void updateNpcStat(int npcId, String stat, String value){
         final L2NpcInstance npc = L2ObjectsStorage.getByNpcId(npcId);
         if (npc != null){
             final L2NpcTemplate template = npc.getTemplate();
             switch (stat){
-                case NpcResource.LEVEL: {template.level = (byte) value;break;}
-                case NpcResource.HP:{template.baseHpMax = (float) value;break;}
-                case NpcResource.MP: {template.baseMpMax = (float) value;break;}
-                case NpcResource.PATK: {template.basePAtk = (int) value;break;}
-                case NpcResource.PDEF: {template.basePDef = (int) value;break;}
-                case NpcResource.MATK: {template.baseMAtk = (int) value;break;}
-                case NpcResource.MDEF: {template.baseMDef = (int) value;break;}
-                case NpcResource.ATKSPD: {template.basePAtkSpd = (int) value;break;}
-                case NpcResource.MATKSPD: {template.baseMAtkSpd = (int) value;break;}
-                case NpcResource.WALKSPD: {template.baseWalkSpd = (int) value;break;}
-                case NpcResource.RUNSPD: {template.baseRunSpd = (int) value;break;}
-                case NpcResource.EXP: {template.revardExp = (int) value;break;}
-                case NpcResource.SP: {template.revardSp = (int) value;break;}
-                case NpcResource.STR: {template.baseSTR = (byte) value;break;}
-                case NpcResource.CON: {template.baseCON = (byte) value;break;}
-                case NpcResource.DEX: {template.baseDEX = (byte) value;break;}
-                case NpcResource._INT: {template.baseINT = (byte) value;break;}
-                case NpcResource.WIT: {template.baseWIT = (byte) value;break;}
-                case NpcResource.MEN: {template.baseMEN = (byte) value;break;}
+                case NpcResource.LEVEL: {template.level = Byte.parseByte(value);break;}
+                case NpcResource.HP:{template.baseHpMax = Float.parseFloat(value);break;}
+                case NpcResource.MP: {template.baseMpMax = Float.parseFloat(value);break;}
+                case NpcResource.PATK: {template.basePAtk = Integer.parseInt(value);break;}
+                case NpcResource.PDEF: {template.basePDef = Integer.parseInt(value);break;}
+                case NpcResource.MATK: {template.baseMAtk = Integer.parseInt(value);break;}
+                case NpcResource.MDEF: {template.baseMDef = Integer.parseInt(value);break;}
+                case NpcResource.ATKSPD: {template.basePAtkSpd = Integer.parseInt(value);break;}
+                case NpcResource.MATKSPD: {template.baseMAtkSpd = Integer.parseInt(value);break;}
+                case NpcResource.WALKSPD: {template.baseWalkSpd = Integer.parseInt(value);break;}
+                case NpcResource.RUNSPD: {template.baseRunSpd = Integer.parseInt(value);break;}
+                case NpcResource.EXP: {template.revardExp = Integer.parseInt(value);break;}
+                case NpcResource.SP: {template.revardSp = Integer.parseInt(value);break;}
+                case NpcResource.STR: {template.baseSTR = Byte.parseByte(value);break;}
+                case NpcResource.CON: {template.baseCON = Byte.parseByte(value);break;}
+                case NpcResource.DEX: {template.baseDEX = Byte.parseByte(value);break;}
+                case NpcResource._INT: {template.baseINT = Byte.parseByte(value);break;}
+                case NpcResource.WIT: {template.baseWIT = Byte.parseByte(value);break;}
+                case NpcResource.MEN: {template.baseMEN = Byte.parseByte(value);break;}
 
             }
         }
